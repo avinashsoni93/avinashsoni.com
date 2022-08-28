d9bd9aa190674669903be78c7384f63ececd9c73
Avinash Soni <avinashsoni@Avinashs-MacBook-Pro.local>
2022-08-28 10:26:16 +0530

    Remove ContactMe component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Skills from "./pages/Skills";
-import ContactMe from "./pages/ContactMe";
 import Timeline from "./pages/Timeline";
 import Projects from "./pages/Projects";
 import Footer from "./components/Footer";
@@ -14,7 +13,6 @@ function App() {
       <Skills />
       <Timeline />
       <Projects />
-      {/* <ContactMe /> */}
       <Footer />
     </div>
   );
